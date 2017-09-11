a5ba3c60f78efa59fe936e0e60869c76d0af188f
Narayanan K <narayanankarthik22@gmail.com>
2017-09-11 21:49:26 +0530

    Add files via upload

---
+++ DisplayDate.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class DisplayDate {
+   public static void main(String args[]) {
+       // Instantiate an object
+       Date date = new Date();
+        
+       // display current timestamp
+       System.out.println(date.getTime());
+   }
+}
